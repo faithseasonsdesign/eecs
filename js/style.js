0b3a140fcd256c55c4330151fdd6040707b426ff
faithseasonsdesign <67489226+faithseasonsdesign@users.noreply.github.com>
2022-02-17 14:21:41 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -20,9 +20,20 @@ function styling(){
     if(window_height <= 568){
         let mask = document.getElementsByClassName("landing-section-mask");
         let section = document.getElementsByClassName("landing-section-bg-image");
+        let impactSection = document.getElementsByClassName("impact-bg-image");
+        let impactMask = document.getElementsByClassName("impact-mask");
         mask[0].style.height= "110vh";
         section[0].style.height ="110vh";
+        impactSection[0].style.height = "160vh";
+        impactMask[0].style.height = "160vh";
         //welcome_caption[0].style.fontSize = "14pt";
+    }
+
+    if(window_height==640 && window_width ==360){
+        let impactSection = document.getElementsByClassName("impact-bg-image");
+        let impactMask = document.getElementsByClassName("impact-mask");
+        impactSection[0].style.height = "120vh";
+        impactMask[0].style.height = "120vh";
     }
    
 }
